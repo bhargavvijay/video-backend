91e78d603147bd39f664a308e4c78ad1fb49609a
Your Name <you@example.com>
2024-11-27 15:07:49 +0530

    Your commit message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./db');
 const path = require('path'); // Import the path module
 const uploadAudioRoute = require('./routes/uploadAudioRoute');
+const cors = require('cors'); // Import cors
 require('dotenv').config();
 
 connectDB();
@@ -10,6 +11,12 @@ const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Enable CORS
+app.use(cors({
+    origin: 'https://bhargavvijay.github.io', // Replace with your frontend origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+}));
 
 // Serve uploaded audio files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
